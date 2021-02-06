a18b6721b77b95f8f678c355967334eedfe2bcc6
Luiz Chequini <luizchequini@gmail.com>
2021-02-06 19:46:17 -0300

    Desenvolvi todos os métodos de Listar todos, Listar por Id, Cadastrar, Editar e Excluir Categorias dentro da Controller responsável pelas categorias. Todas as Api froam testadas com JWT dentro do Postman.

--- BlogPessoalApi/WebAPI/Controllers/CategoriaApiController.cs
+++ BlogPessoalApi/WebAPI/Controllers/CategoriaApiController.cs
@@ -2,14 +2,13 @@
 using Data.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
     [Authorize]
+    [Route("api/categoria")]
+    [ApiController]
     public class CategoriaApiController : Controller
     {
         private readonly ICategoria _iCategoria;
@@ -19,16 +18,57 @@ namespace WebAPI.Controllers
             _iCategoria = iCategoria;
         }
 
-        [HttpGet("/api/ListarCategorias")]
+        [HttpGet("ListarCategorias")]
         public async Task<JsonResult> ListaCategorias()
         {
             return Json(await this._iCategoria.List());
         }
 
-        [HttpPost("/api/CadastrarCategoria")]
+        [HttpGet("ListarCategoria/{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            Categoria categoria = await _iCategoria.GetEntityById(id);
+
+            if (categoria == null)
+            {
+                return NotFound("Não foi possível encontrar o registro de Categoria!");
+            }
+
+            return Ok(categoria);
+        }
+
+        [HttpPost("CadastrarCategoria")]
         public async Task CadastrarCategoria([FromBody] Categoria categoria)
         {
             await Task.FromResult(this._iCategoria.Add(categoria));
+        }
+
+        [HttpPut("AtualizarCategoria/{id}")]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] Categoria categoria)
+        {
+            if (categoria == null)
+            {
+                return BadRequest("Categoria é nula!");
+            }
+            Categoria categoriaAtualizar = await _iCategoria.GetEntityById(id);
+            if (categoriaAtualizar == null)
+            {
+                return NotFound("Categoria não foi encontrada!");
+            }
+            await _iCategoria.Update(categoria);
+            return NoContent();
+        }
+
+        [HttpDelete("ExcluirCategoria/{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            Categoria categoria = await _iCategoria.GetEntityById(id);
+            if (categoria == null)
+            {
+                return BadRequest("Cliente não encontrado!");
+            }
+            await _iCategoria.Delete(categoria);
+            return NoContent();
         }
     }
 }
